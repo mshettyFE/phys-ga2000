541a5fbb5466fdaf97bd356cdeb89d78e69c3c61
Mihir Shetty <ms6556@columbia.edu>
2024-10-14 10:24:06 -0400

    Finished SVD eigenvalue decomp

--- ps-6/PCAAnalysis.py
+++ ps-6/PCAAnalysis.py
@@ -1,6 +1,8 @@
 from astropy.io import fits
 import numpy as np
+import scipy
 import matplotlib.pyplot as plt
+import time
 
 def read_data():
     hdu_list = fits.open("specgrid.fits")
@@ -9,7 +11,7 @@ def read_data():
     return logwave, flux
 
 def plot_random(logwave, flux):
-    galaxies = np.random.choice(flux.shape[1],5)
+    galaxies = np.random.choice(flux.shape[0],5)
     counter = 0
     for galaxy in galaxies:
         plt.clf() 
@@ -25,8 +27,50 @@ def RemoveMean(flux):
     mean = np.mean(flux, axis=1, keepdims = True)
     return flux-mean, mean
 
+def DirectEigen(residuals):
+    C = np.matmul(np.transpose(residuals), residuals)*(1.0/residuals.shape[0])
+    t1 = time.time()
+    value, vectors =  np.linalg.eig(C)
+    t2 = time.time()
+    return value, vectors, t2-t1
+
+def plot_eigenvectors(logwave, eigen_matrix):
+    counter = 0
+    for galaxy in range(0,5):
+        plt.clf() 
+        plt.plot(logwave, flux[galaxy,:])
+        plt.savefig("PartD_Eigen_"+str(counter)+".png")
+        counter += 1
+
+def SVD_Eigen(residuals):
+    t1 = time.time()
+    U,W,V = scipy.linalg.svd(residuals)
+    t2 = time.time()
+    return W*W, V, t2-t1
+
+def plot_eigenvectors_SVD(logwave, eigen_matrix):
+    counter = 0
+    for galaxy in range(0,5):
+        plt.clf() 
+        plt.plot(logwave, flux[galaxy,:])
+        plt.savefig("PartE_Eigen_"+str(counter)+".png")
+        counter += 1
+
 if __name__ == "__main__":
     logwave, flux = read_data()
+# A
     plot_random(logwave, flux)
+# B
     normed_flux, norm_constants = Normalization(flux)
-    centered_normed_flux, mean = RemoveMean(normed_flux)
+# C
+    residuals, mean = RemoveMean(normed_flux)
+# D
+#    values, vectors, time_taken = DirectEigen(residuals)
+#    plot_eigenvectors(logwave, vectors)
+#    print("Time Taken: {t}. Condition Number: {cond}".format(t=time_taken, cond = np.max(values)/np.min(values)))
+# E
+    eigen_values, eigenvectors, time_taken_SVD = SVD_Eigen(residuals)
+#    print(eigenvectors.shape)
+#    plot_eigenvectors_SVD(logwave, eigenvectors)
+#    print("Time Taken: {t}. Condition Number: {cond}".format(t=time_taken_SVD, cond = np.max(eigen_values)/np.min(eigen_values)))
+    print(eigen_values.shape)
