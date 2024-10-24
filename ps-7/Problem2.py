4eeff9745b6a6969ee23e428e5cf3319acc9fed9
Mihir Shetty <ms6556@columbia.edu>
2024-10-24 13:49:31 -0400

    Got golden section working. Need to do Brent's now

--- ps-7/Problem2.py
+++ ps-7/Problem2.py
@@ -1,21 +1,29 @@
 import numpy as np
 from  scipy.optimize import brent as sp_brent
 
-def brent(f, a, b, c, max_iters = 500, tol =1E-4):
-    assert(f(b) < f(a))
+def golden_section_step(a,b,c):
+    w = (3-np.sqrt(5))/2
+    z = 1-2*w
+    b = w*(c-a)+a
+    x = z*(c-a)+b
+    if (f(x) < f(b)):
+# Shift left boundary over
+        a = b
+    else:
+# Shift right boundary over
+        c = b
+    b = w*(c-a)+a
+    return (a,b,c)
+
+def brent(f, a, c, max_iters = 500, tol =1E-4):
+    assert(c > a)
+    w = (3-np.sqrt(5))/2
+    z = 1-2*w
+    b = w*(c-a)+a
     assert(f(b) < f(c))
+    assert(f(a) < f(c))
     for i in range(max_iters):
-        print(a,b,c)
-        w = (b-a)/(c-a)
-        x_new = (1-2*w)*(c-a)+b
-        print(w, x_new)
-        if(abs(x_new-b) < 1E-4):
-            break
-        if x_new > b:
-            a = b
-        else:
-            c = b
-        b = (c+a)/2
+        a,b,c = golden_section_step(a,b,c)
     return b
 
 def f(x):
@@ -24,5 +32,5 @@ def f(x):
 
 
 if __name__ == "__main__":
-    print(brent(f, 0,0.5,1))
+    print(brent(f, 0,1))
     print(sp_brent(f,brack=(0,1)))
