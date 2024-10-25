3900d6592c29d19d2dbabcb1691b982dfa6f120c
Mihir Shetty <ms6556@columbia.edu>
2024-10-25 13:48:44 -0400

    Got Problem2 working

--- ps-7/Problem2.py
+++ ps-7/Problem2.py
@@ -1,9 +1,14 @@
 import numpy as np
 from  scipy.optimize import brent as sp_brent
 
-def golden_section_step(a,b,c):
-    w = (3-np.sqrt(5))/2
-    z = 1-2*w
+w = (3-np.sqrt(5))/2
+z = 1-2*w
+
+def golden_section_step(f,a,b,c):
+    if (c<a):
+        temp = a
+        a = c
+        c =temp
     b = w*(c-a)+a
     x = z*(c-a)+b
     if (f(x) < f(b)):
@@ -15,20 +20,42 @@ def golden_section_step(a,b,c):
     b = w*(c-a)+a
     return (a,b,c)
 
-def brent(f, a, c, max_iters = 500, tol =1E-4):
+def parabolic_step(func=None, a=None, b=None, c=None):
+    """returns the minimum of the function as approximated by a parabola"""
+    fa = func(a)
+    fb = func(b)
+    fc = func(c)
+    denom = (b - a) * (fb - fc) - (b -c) * (fb - fa)
+    numerator = (b - a)**2 * (fb - fc) - (b -c)**2 * (fb - fa)
+    # If singular, just return b 
+    if(np.abs(denom) < 1.e-15):
+        x = b
+    else:
+        x = b - 0.5 * numerator / denom
+    return(x)
+
+def brent(f, a, c, max_iters = 5000, tol =1E-4):
     assert(c > a)
-    w = (3-np.sqrt(5))/2
-    z = 1-2*w
     b = w*(c-a)+a
     assert(f(b) < f(c))
     assert(f(a) < f(c))
     for i in range(max_iters):
-        a,b,c = golden_section_step(a,b,c)
-    return b
+        x = parabolic_step(f,a,b,c)
+        if x!=b:
+            if (b < x):
+# Shift left boundary over
+                c = b
+            else:
+# Shift right boundary over
+                a = b
+            b= x
+        else:
+            a,b,c = golden_section_step(f,a,b,c)
+    return (a+c)/2
 
 def f(x):
-#    return np.power(x-0.3,2)*np.exp(x)
-    return np.power(x-0.3,2)
+    return np.power(x-0.3,2)*np.exp(x)
+#    return np.power(x-0.3,2)
 
 
 if __name__ == "__main__":
